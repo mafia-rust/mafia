2b308908bcf1f22534ffa2e9eea81623e75ec773
Jack Papel <jackpapel@live.com>
2023-01-27 12:37:28 -0500

    Refactor, and follow rust naming conventions

---
+++ mafia_server/src/game/phase.rs
@@ -0,0 +1,28 @@
+// This is a placeholder for now
+pub type PhaseTime = std::time::Duration;
+
+pub enum PhaseType {
+    Morning, 
+    Discussion, 
+    Voting, 
+    Testimony, 
+    Judgement, 
+    FinalWords, 
+    Night
+}
+
+pub struct Phase {
+    phase_type : PhaseType,
+    number: u8, // Hopefully nobody is having more than 256 days anyway
+    length : Duration,
+}
+
+impl Phase {
+    pub fn start(&self) {
+        // Match phase type and do stuff
+    }
+
+    pub fn end(&self) {
+        // Match phase type and do stuff
+    }
+}
---
+++ mafia_server/src/main.rs
@@ -0,0 +1,28 @@
+use mafia_server::lobby::Lobby;
+
+fn main() {
+    println!("Hello, world!");
+
+    let lobbies: Vec<Lobby>;    // Multiple lobbies? For what?
+
+}
+
+
+/*
+    options for internet communication.
+    Option 1: 
+        What i did last time, have different set message types.
+        Each message type allows you to run a function on the other machine.
+        Very generic
+
+    Option 2:
+        Syncronize values across both. 
+        Have a way to just say, i want these 2 values to be in sync, then whenever it changes on the server, it updates the clients.
+        This requires the concept of a machine to own a value.
+            If the server owns the value then it gets to change it
+            If the client owns the value then it gets to change it
+
+    Option 3:
+        Both. Do both of them at the same time.
+
+*/
