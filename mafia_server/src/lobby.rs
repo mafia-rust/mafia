951d1734f82198a7ec59b85e961d70c765164734
Sam Maselli <sammypet3@gmail.com>
2023-01-29 13:42:45 -0500

    tls

--- mafia_server/src/lobby.rs
+++ mafia_server/src/lobby.rs
@@ -22,46 +22,54 @@ impl Lobby{
     }
 }
 
-use futures_util::{StreamExt, TryStreamExt, future, pin_mut};
-use tokio_tungstenite::tungstenite;
-
 use std::{
     collections::HashMap,
-    io::Error as IoError,
+    io,
+    io::Read,
+    fs::File,
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
 
+use futures_util::{StreamExt, TryStreamExt, future, pin_mut};
+use tokio_tungstenite::tungstenite::{self, stream};
+
 use tokio::net::{TcpListener, TcpStream};
 use tungstenite::protocol::Message;
 
-use tokio_native_tls::native_tls::{Identity, TlsAcceptor};
+use tokio_native_tls::native_tls::{Identity};
+use tokio_native_tls::{TlsAcceptor, TlsStream};
 
 #[tokio::main]
-async fn create_ws_server() -> Result<(), IoError> {
+async fn create_ws_server() -> core::result::Result<(), io::Error> {
+    
+    //https://docs.rs/native-tls/latest/native_tls/
+    let mut file = File::open("identity.pfx").unwrap();
+    let mut identity = vec![];
+    file.read_to_end(&mut identity).unwrap();
+
+    let identity = Identity::from_pkcs12(&identity, "hunter2").unwrap();
+    let acceptor = TlsAcceptor::from(tokio_native_tls::native_tls::TlsAcceptor::new(identity).unwrap());
+    let acceptor = Arc::new(acceptor);
+
+
     // Server address
     let addr = "127.0.0.1:8081";
-
-
     // Create the event loop and TCP listener we'll accept connections on.
     let listener = TcpListener::bind(&addr).await.unwrap();
     println!("Listening on: {}", addr);
 
-    //https://docs.rs/native-tls/latest/native_tls/
-    let identity = Identity::from_pkcs12(&identity, "hunter2").unwrap();
-    let acceptor = TlsAcceptor::new(identity).unwrap();
-    let acceptor = Arc::new(acceptor);
-
-
     // Handle each incoming connection in a separate task
     while let Ok((stream, addr)) = listener.accept().await {
+        let acceptor = acceptor.clone();
+        let stream = acceptor.accept(stream).await.unwrap();
         tokio::spawn(handle_connection(stream, addr));
     }
 
     Ok(())
 }
 
-async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr) {
+async fn handle_connection(raw_stream: TlsStream<TcpStream>, addr: SocketAddr) {
     println!("Incoming TCP connection from: {}", addr);
 
     // Upgrade the raw stream to a WebSocket stream
