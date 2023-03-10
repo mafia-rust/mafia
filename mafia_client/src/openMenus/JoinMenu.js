6704aebfe175737913147d42031e47aa09e435c4
Jack Papel <jackpapel@Hephaestus>
2023-03-10 00:32:55 -0500

    Separate common CSS, basic JoinMenu

--- mafia_client/src/openMenus/JoinMenu.js
+++ mafia_client/src/openMenus/JoinMenu.js
@@ -1,42 +1,70 @@
 import React from "react";
 import gameManager from "../index.js";
+import "../index.css";
+import "./joinMenu.css";
 
 export class JoinMenu extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            roomCodeValue: "",
+            roomCode: "",
+            name: "",
         };
     }
     componentDidMount() {
     }
     componentWillUnmount() {
     }
+    setRoomCode(code) {
+        this.setState({roomCode: code})
+    }
+    setName(name) {
+        this.setState({name: name})
+    }
     joinGameButton(){
-        gameManager.roomCode = Number(this.state.roomCodeValue);
+        gameManager.roomCode = Number(this.state.roomCode);
+        gameManager.name = this.state.name;
+
         gameManager.Server.close();
         gameManager.Server.open();
         // Wait for server to open
         
         setTimeout(gameManager.join_button);
     }
-    hostGameButton(){
-        gameManager.Server.close();
-        gameManager.Server.open();
-        // Wait for server to open
+    // hostGameButton(){
+    //     gameManager.Server.close();
+    //     gameManager.Server.open();
+    //     // Wait for server to open
         
-        setTimeout(gameManager.host_button);
-    }
-    render(){return(<div>
-        Room Code<br/>
-        <input type="text" value={this.state.roomCodeValue} 
-            onChange={(e)=>{this.setState({roomCodeValue: e.target.value})}}
-            onKeyUp={(e)=>{
-                if(e.key === 'Enter')
-                    gameManager.roomCode = Number(this.state.roomCodeValue);
-            }}
-        />
-        <button style={{width: "90%"}} onClick={()=>{this.button(false)}}>Join Lobby</button><br/>
+    //     setTimeout(gameManager.host_button);
+    // }
+    render(){return(<div style={{display: "flex", flexDirection: "column"}}>
+        <div className="header jm-header">
+            <h1 className="header-text jm-header-text">Join Game</h1>
+        </div>
+        <div className="jm-input-column">
+            <div className="input-box">
+                <h3 className="input-box-label">Room code</h3>
+                <input className="input-field" type="text" value={this.state.roomCode} 
+                    onChange={(e)=>{this.setRoomCode(e.target.value)}}
+                    onKeyUp={(e)=>{
+                        if(e.key === 'Enter')
+                            gameManager.roomCode = Number(this.state.roomCode);
+                    }}
+                />
+            </div>
+            <div className="input-box">
+                <h3 className="input-box-label">Name</h3>
+                <input className="input-field" type="text" value={this.state.name} 
+                    onChange={(e)=>{this.setName(e.target.value)}}
+                    onKeyUp={(e)=>{
+                        if(e.key === 'Enter')
+                            gameManager.name = this.state.name;
+                    }}
+                />
+            </div>
+            <button className="button jm-button" onClick={()=>{this.joinGameButton()}}>Join Lobby</button>
+        </div>
     </div>)}
 }
--- mafia_client/src/openMenus/StartMenu.js
+++ mafia_client/src/openMenus/StartMenu.js
@@ -3,6 +3,7 @@ import gameManager from "../index.js";
 import { create_gameState } from "../game/gameState";
 import {Main} from "../Main";
 import { JoinMenu } from "./JoinMenu";
+import "../index.css"
 import "./startMenu.css"
 
 export class StartMenu extends React.Component {
@@ -18,19 +19,19 @@ export class StartMenu extends React.Component {
     componentWillUnmount() {
     }
     render(){return(<div>
-        <div className="header">
-            <h1 className="title">Mafia</h1>
-            <button className="login-button">Login</button><br/>
+        <div className="header sm-header">
+            <h1 className="header-text">Mafia</h1>
+            <button className="button sm-login-button">Login</button><br/>
         </div>
 
-        <div className="button-area">
-            <button className="join-host-button" onClick={()=>{
+        <div className="sm-button-area">
+            <button className="button sm-join-host-button" onClick={()=>{
                 gameManager.gameState = create_gameState();
                 Main.instance.setContent(<JoinMenu/>);
             }}>{Main.instance?.isLoggedIn() ? "Join" : "Join as guest"}</button>
-            <button className="join-host-button" onClick={()=>{
-                gameManager.gameState = create_gameState();
-                Main.instance.setContent(<JoinMenu/>);
+            <button className="button sm-join-host-button" onClick={()=>{
+                // Create gamestate, generate lobby,
+                // and enter LobbyMenu screen
             }}>{Main.instance?.isLoggedIn() ? "Host" : "Host as guest"}</button>
         </div>
 
