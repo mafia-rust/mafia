b6ce02ad16bdb703439da4d9cc85dbe0680b9595
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-03-22 23:39:08 -0400

    added the fancy moving dots
    added the fancy dots that bounce up and down on the loading screen

--- mafia_client/src/openMenus/LoadingScreen.tsx
+++ mafia_client/src/openMenus/LoadingScreen.tsx
@@ -7,10 +7,14 @@ export const enum Type {
     Join = "join",
     Default = "default"
 }
-
+//uses the index.css files' loader class to create loading dots after 
+//the text "Loading" or "Joining" or "Hosting"
 export function create(value: Type = Type.Default) {
     return <div className="header" style={{height: "100%"}}>
     <h1 className="header-text">
         {translate("menu.loading." + value)}
+        <br></br>
+        <br></br>
+        <span className="loading-dots"></span>
     </h1>
 </div>}
