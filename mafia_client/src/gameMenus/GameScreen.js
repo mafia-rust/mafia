111c0c3b988a4d788289b43531f8169c74dc04e4
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-03-23 02:20:17 -0400

    moved style out of GameScreen.js

--- mafia_client/src/gameMenus/GameScreen.js
+++ mafia_client/src/gameMenus/GameScreen.js
@@ -1,72 +1,76 @@
 import React from "react";
-import "../index.css"
+import "../index.css";
 import * as LoadingScreen from "../openMenus/LoadingScreen";
 import { PhaseRowMenu } from "./PhaseRowMenu";
 import { GraveyardMenu } from "./GraveyardMenu";
 import { ChatMenu } from "./ChatMenu";
 import { PlayerListMenu } from "./PlayerListMenu";
 import { WillMenu } from "./WillMenu";
+import "./gameScreen.css";
 
 export class GameScreen extends React.Component {
     static instance;
+
     constructor(props) {
         super(props);
-
         this.state = {
             header: LoadingScreen.create(),
             content: [LoadingScreen.create()],
         };
     }
+
     componentDidMount() {
         GameScreen.instance = this;
         this.setState({
-            header : <PhaseRowMenu/>,
-            content : [<GraveyardMenu/> ,<ChatMenu/>, <PlayerListMenu/>, <WillMenu/>],
+            header: <PhaseRowMenu/>,
+            content: [
+                <GraveyardMenu/>,
+                <ChatMenu/>,
+                <PlayerListMenu/>,
+                <WillMenu/>
+            ],
         });
     }
+
     componentWillUnmount() {
         //GameScreen.instance = undefined;
     }
 
-    render(){return(<div style={{
-        height: "100vh"
-    }}>
-        {this.renderHeader(this.state.header)}
-        {this.renderContent(this.state.content)}
-    </div>)}
+    render() {
+        return (
+            <div className="game-screen">
+                {this.renderHeader(this.state.header)}
+                {this.renderContent(this.state.content)}
+            </div>
+        );
+    }
 
-    renderHeader(header){return(<div>
-        {header}
-    </div>)}
-    renderContent(content){return(<div style={{
-        display: "grid",
+    renderHeader(header) {
+        return (
+            <div className="game-screen-header">
+                {header}
+            </div>
+        );
+    }
 
-        gridAutoColumns: "1fr",
-        gridAutoRows: "1fr",
-
-        height: "85%",
-        width: "100%",
-
-        backgroundColor: "black",
-        gridGap: "5px",
-    }}>
-        {
-            content.map((panel, index)=>{
-                return (<div 
-                key={index}
-                style={{
-                    gridColumn: (index+1),
-                    gridRow: 1,
-                    
-                    overflowX: "hidden",
-                    height : "100%",
-                    width: "100%",
-                    
-                    backgroundColor: "green",
-                }}>
-                    {panel}
-                </div>)
-            })
-        }
-    </div>)}
+    renderContent(content) {
+        return (
+            <div className="game-screen-content">
+                {content.map((panel, index) => {
+                    return (
+                        <div
+                            key={index}
+                            className="game-screen-panel"
+                            style={{
+                                gridColumn: index + 1,
+                                gridRow: 1,
+                            }}
+                        >
+                            {panel}
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
