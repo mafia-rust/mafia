3e2b79018d0a61cf066c2fbf0ba27854e1e81318
isaac worsencroft <47464523+ize69@users.noreply.github.com>
2023-03-26 20:25:59 -0400

    auto test devlupment

--- auto_test/auto_test.bat
+++ auto_test/auto_test.bat
@@ -1,23 +1,47 @@
 @echo off
 
 set /a tabs=0
+set SERVER_PORT=
 
+REM Check if mafia_server is running on localhost:3000
+powershell -Command "Test-NetConnection -ComputerName 'localhost' -Port 3000 -InformationLevel Quiet" > nul
+if %ERRORLEVEL% EQU 0 (
+  set SERVER_PORT=3000
+)
+
+REM Find port number of mafia_server.exe process using netstat
+if not defined SERVER_PORT (
+  for /f "tokens=1,2,3" %%a in ('netstat -ano ^| find "LISTENING" ^| find "127.0.0.1"') do (
+      for /f "tokens=1,2" %%x in ('tasklist /FI "PID eq %%c" /FO CSV /NH') do (
+          if "%%y" == "mafia_server.exe" (
+              set SERVER_PORT=%%b
+          )
+      )
+  )
+)
+
+if not defined SERVER_PORT (
+    echo Mafia server is not running!
+    exit /b 1
+)
+
+echo Mafia server is running on port %SERVER_PORT%
 :loop
-set /p choice=Enter choice (a=add tab, c=close, e=exit, n=new window, s=start game): 
+set /p choice=Enter choice (s=start, c=close, e=exit, n=new window): 
 
-if /i "%choice:~0,1%"=="a" (
+if /i "%choice:~0,1%"=="s" (
     set /a num=%choice:~1%
     if not defined num set num=1
     for /l %%x in (1,1,%num%) do (
         set /a tabs+=1
-        start chrome http://localhost:3000/
+        start chrome http://localhost:%SERVER_PORT%/
     )
     goto loop
 )
 
 if /i "%choice:~0,1%"=="c" (
     if %tabs% gtr 0 (
-        taskkill /im chrome.exe /fi "windowtitle eq http://localhost:3000/*"
+        taskkill /im chrome.exe /fi "windowtitle eq http://localhost:%SERVER_PORT%/*"
         set /a tabs-=1
     )
     goto loop
@@ -28,12 +52,12 @@ if /i "%choice:~0,1%"=="n" (
     if not defined num set num=1
     set /a num-=1
     if %num% gtr 0 (
-        start chrome http://localhost:3000/ --new-window
+        start chrome http://localhost:%SERVER_PORT%/ --new-window
         for /l %%x in (1,1,%num%) do (
-            start chrome http://localhost:3000/
+            start chrome http://localhost:%SERVER_PORT%/
         )
     ) else (
-        start chrome http://localhost:3000/ --new-window
+        start chrome http://localhost:%SERVER_PORT%/ --new-window
     )
     goto loop
 )
---
+++ auto_test/auto_test.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def make_host(driver):
+    # wait for the Host button to be clickable
+    wait = WebDriverWait(driver, 10)
+    host_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Host')]")))
+    host_button.click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Start Game')]")))
+def make_reg_players(driver):
+    # wait for the Host button to be clickable
+    wait = WebDriverWait(driver, 10)
+    host_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Join')]")))
+    host_button.click()
+    #wait till page contains changes 
+    wait = WebDriverWait(driver, 10)
+    host_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Join Lobby')]")))
+    host_button.click()
+def main(players,auto_start):
+    driver = webdriver.Chrome()
+    # navigate to the Mafia game page
+    driver.get("http://localhost:3000/")
+    # wait for the page to load
+    
+    make_host(driver)
+    #makes a new tab and runs the make_reg_players function for each player
+    for i in range(players-1):
+        driver.execute_script("window.open('');")
+        driver.switch_to.window(driver.window_handles[i+1])
+        driver.get("http://localhost:3000/")
+        make_reg_players(driver)
+    driver.switch_to.window(driver.window_handles[0])
+    
+    while(True):
+        time.sleep(5)
+
+    
+
+if __name__ == "__main__":
+    main(16,False)
