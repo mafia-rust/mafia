1c906af99af5942f7259f52613bea2bcb634d0a8
Jack Papel <jackpapel@Hephaestus>
2023-04-04 16:29:08 -0400

    See previous

---
+++ mafia_client/src/menu/lobby/LobbyMenu.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GAME_MANAGER from "../../index";
+import LobbyPlayerList from "./LobbyPlayerList";
+import LobbyPhaseTimePane from "./LobbyPhaseTimePane";
+import LobbyRolePane from "./LobbyRolePane";
+import "./lobbyMenu.css";
+import translate from "../../game/lang";
+
+export function create() {
+    return <div style={{
+        display: "flex",
+        flexDirection: "column",
+    }}>
+        <div className="header lm-header">
+            <h1 className="header-text lm-header-text">
+                {translate("menu.lobby.title", GAME_MANAGER.roomCode)}
+            </h1>
+            <button className="button lm-start-button" onClick={GAME_MANAGER.startGame_button}>
+                {translate("menu.lobby.button.start")}
+            </button>
+        </div>
+        <div style={{
+            display: "flex",
+            flexDirection: "row",
+        }}>
+            <LobbyPlayerList/>
+            <div className="lm-settings-column">
+                <h2 className="lm-settings-header">Game settings</h2>
+                <LobbyPhaseTimePane/>
+                <LobbyRolePane/>
+            </div>
+
+        </div>
+    </div>
+}
