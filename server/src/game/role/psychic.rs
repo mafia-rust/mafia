2c90976450fc5b75d7bc52d922d0a179ecc84279
Sam Maselli <sammypet3@gmail.com>
2024-01-02 21:42:58 -0500

    fix psychic seeing messages when theyre dead

--- server/src/game/role/psychic.rs
+++ server/src/game/role/psychic.rs
@@ -30,6 +30,7 @@ impl RoleStateImpl for Psychic {
     fn do_night_action(self, game: &mut Game, actor_ref: PlayerReference, priority: Priority) {
         if actor_ref.night_jailed(game) {return;}
         if actor_ref.night_roleblocked(game) {return;}
+        if !actor_ref.alive(game) {return;}
 
         if priority != Priority::Investigative {return}
 
