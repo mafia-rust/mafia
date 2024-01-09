8546449529a0bb01983f84bf00f98700e17a7402
Sam Maselli <sammypet3@gmail.com>
2024-01-09 00:56:43 -0500

    psychic code is now legible

--- server/src/game/role/psychic.rs
+++ server/src/game/role/psychic.rs
@@ -32,67 +32,15 @@ impl RoleStateImpl for Psychic {
         if actor_ref.night_roleblocked(game) {return;}
 
         if priority != Priority::Investigative {return}
-
-        let all_players: Vec<_> = PlayerReference::all_players(game)
-            .into_iter()
-            .filter(|p|p.alive(game)&&*p!=actor_ref)
-            .collect();
-
-        let mut rng = rand::thread_rng();
-        actor_ref.push_night_message(game, 'a: {match game.day_number() % 2 {
+        
+        actor_ref.push_night_message(game, match game.day_number() % 2 {
             1=>{
-                //evil vision
-                let all_non_townies: Vec<_> = PlayerReference::all_players(game)
-                    .into_iter()
-                    .filter(|p|p.alive(game)&&*p!=actor_ref&&p.night_appeared_role(game).faction_alignment().faction()!=Faction::Town)
-                    .collect();
-
-                if let Some(non_townie) = all_non_townies.choose(&mut rng){
-                    let random_players: Vec<PlayerReference> = all_players.into_iter()
-                        .filter(|p|p!=non_townie)
-                        .collect::<Vec<_>>()
-                        .choose_multiple(&mut rng, 2)
-                        .copied()
-                        .collect();
-                    
-                    if let Some(random_player0) = random_players.get(0){
-                        if let Some(random_player1) = random_players.get(1){
-
-                            let mut out = [non_townie, random_player0, random_player1];
-                            out.shuffle(&mut rng);
-                            break 'a ChatMessage::PsychicEvil { players: [out[0].index(), out[1].index(), out[2].index()] }
-                        }
-                    }
-                }
-                ChatMessage::PsychicFailed
+                Psychic::get_psychic_result_evil(game, actor_ref)
             },
             _=>{
-                //good vision
-                let all_townies: Vec<_> = PlayerReference::all_players(game)
-                    .into_iter()
-                    .filter(|p|p.alive(game)&&*p!=actor_ref&&p.night_appeared_role(game).faction_alignment().faction()==Faction::Town)
-                    .collect();
-
-                if let Some(townie) = all_townies.choose(&mut rng){
-                    if let Some(random_player) = all_players.into_iter()
-                        .filter(|p|p!=townie)
-                        .collect::<Vec<_>>()
-                        .choose(&mut rand::thread_rng()){
-                        
-                        let mut out = [townie, random_player];
-                        out.shuffle(&mut rng);
-
-                        break 'a ChatMessage::PsychicGood { players: [out[0].index(), out[1].index()] }   
-                    }
-                }
-                ChatMessage::PsychicFailed
+                Psychic::get_psychic_result_good(game, actor_ref)
             },
-        }});
-        
-        
-        
-        
-
+        });
     }
     fn can_night_target(self, _game: &Game, _actor_ref: PlayerReference, _target_ref: PlayerReference) -> bool {
         false
@@ -124,4 +72,54 @@ impl RoleStateImpl for Psychic {
     }
     fn on_game_ending(self, _game: &mut Game, _actor_ref: PlayerReference){
     }
+}
+
+impl Psychic {
+    fn get_psychic_result_evil(game: &Game, actor_ref: PlayerReference)->ChatMessage{
+        
+        let mut rng = rand::thread_rng();
+
+        let all_non_townies: Vec<_> = Psychic::get_valid_players(game, actor_ref).into_iter()
+            .filter(|p|p.night_appeared_role(game).faction_alignment().faction()!=Faction::Town) .collect();
+
+        let Some(selected_ref) = all_non_townies.choose(&mut rng) else {return ChatMessage::PsychicFailed};
+
+        let random_players: Vec<PlayerReference> = Psychic::get_valid_players(game, actor_ref).into_iter()
+            .filter(|p|p!=selected_ref).collect::<Vec<_>>()
+            .choose_multiple(&mut rng, 2).copied().collect();
+        
+        let Some(random_player0) = random_players.get(0) else {return ChatMessage::PsychicFailed};
+        let Some(random_player1) = random_players.get(1) else {return ChatMessage::PsychicFailed};
+
+        let mut out = [selected_ref, random_player0, random_player1];
+        out.shuffle(&mut rng);
+        ChatMessage::PsychicEvil { players: [out[0].index(), out[1].index(), out[2].index()] }
+
+    }
+    fn get_psychic_result_good(game: &Game, actor_ref: PlayerReference)->ChatMessage{
+        let mut rng = rand::thread_rng();
+
+        let all_townies: Vec<_> = Psychic::get_valid_players(game, actor_ref).into_iter()
+            .filter(|p|p.night_appeared_role(game).faction_alignment().faction()==Faction::Town).collect();
+
+        let Some(selected_ref) = all_townies.choose(&mut rng) else {return ChatMessage::PsychicFailed};
+
+        let random_players: Vec<_> = Psychic::get_valid_players(game, actor_ref).into_iter()
+            .filter(|p|p!=selected_ref).collect::<Vec<_>>();
+        
+        let Some(random_player) = random_players.choose(&mut rng) else {return ChatMessage::PsychicFailed};
+
+        let mut out = [selected_ref, random_player];
+        out.shuffle(&mut rng);
+        ChatMessage::PsychicGood { players: [out[0].index(), out[1].index()] }
+    }
+
+
+    fn get_valid_players(game: &Game, actor_ref: PlayerReference)->Vec<PlayerReference>{
+        PlayerReference::all_players(game)
+            .into_iter()
+            .filter(|player_ref|player_ref.alive(game) && *player_ref!=actor_ref)
+            .collect()
+    }
+
 }
