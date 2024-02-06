60fc9e75d08d2f2f8e68b211f27b74050d59f2b3
Sam Maselli <sammypet3@gmail.com>
2024-02-06 14:08:26 -0500

    add clocktower online link

--- client/src/menu/game/gameScreenContent/ChatMenu.tsx
+++ client/src/menu/game/gameScreenContent/ChatMenu.tsx
@@ -50,8 +50,8 @@ export default class ChatMenu extends React.Component<ChatMenuProps, ChatMenuSta
             {this.state.filter && <button 
                 onClick={()=>{
                     // TODO: Sammy wtf??
-                    GAME_MANAGER.invokeStateListeners("tick")
-                    ChatMenu.setFilter(null)
+                    GAME_MANAGER.invokeStateListeners("tick");
+                    ChatMenu.setFilter(null);
                 }}
                 className="material-icons-round highlighted"
                 aria-label={translate("menu.chat.clearFilter")}
--- client/src/menu/main/StartMenu.tsx
+++ client/src/menu/main/StartMenu.tsx
@@ -42,9 +42,10 @@ export default class StartMenu extends React.Component<StartMenuProps, StartMenu
             <nav>
                 <ul>
                     <li><a href="https://www.github.com/ItsSammyM/mafia-rust">Github</a></li>
+                    <li onClick={()=>{Anchor.setContent(<StandaloneWiki/>)}}>{translate("menu.wiki.title")}</li>
                     <li><a href="https://mafia-game-old.vercel.app/">Old Mafia</a></li>
                     <li><a href="https://netgames.io/games/">Net Games</a></li>
-                    <li onClick={()=>{Anchor.setContent(<StandaloneWiki/>)}}>{translate("menu.wiki.title")}</li>
+                    <li><a href="https://clocktower.online/">Clocktower Online</a></li>
                 </ul>
             </nav>
         </footer>
