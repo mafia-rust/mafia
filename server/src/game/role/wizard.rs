a387f895ee4704bc0d21f68f430ab6929a9508b6
Alex <113641348+emt7553@users.noreply.github.com>
2024-02-22 01:11:35 -0500

    merged main

---
+++ server/src/game/role/wizard.rs
@@ -0,0 +1,68 @@
+use serde::Serialize;
+
+use crate::game::chat::{ChatGroup, ChatMessage};
+use crate::game::grave::GraveKiller;
+use crate::game::phase::PhaseType;
+use crate::game::player::PlayerReference;
+use crate::game::role_list::Faction;
+use crate::game::visit::Visit;
+use crate::game::team::Team;
+use crate::game::Game;
+use super::{Priority, RoleStateImpl};
+
+
+#[derive(Debug, Clone, Serialize, Default)]
+pub struct Wizard;
+
+pub(super) const FACTION: Faction = Faction::Neutral;
+pub(super) const MAXIMUM_COUNT: Option<u8> = Some(0);
+
+impl RoleStateImpl for Wizard {
+    fn defense(&self, _game: &Game, _actor_ref: PlayerReference) -> u8 {0}
+    fn team(&self, _game: &Game, _actor_ref: PlayerReference) -> Option<Team> {Some(Team::Mafia)}
+
+
+    fn do_night_action(self, game: &mut Game, actor_ref: PlayerReference, priority: Priority) {
+        if priority != Priority::Kill {return}
+        
+        if let Some(visit) = actor_ref.night_visits(game).first(){
+            let target_ref = visit.target;
+            if target_ref.night_jailed(game){
+                actor_ref.push_night_message(game, ChatMessage::TargetJailed);
+                return
+            }
+    
+            target_ref.try_night_kill(actor_ref, game, GraveKiller::Faction(Faction::Mafia), 1, true);
+        }
+    }
+    fn can_night_target(self, game: &Game, actor_ref: PlayerReference, target_ref: PlayerReference) -> bool {
+        crate::game::role::common_role::can_night_target(game, actor_ref, target_ref)
+    }
+    fn do_day_action(self, _game: &mut Game, _actor_ref: PlayerReference, _target_ref: PlayerReference) {
+    
+    }
+    fn can_day_target(self, _game: &Game, _actor_ref: PlayerReference, _target_ref: PlayerReference) -> bool {
+        false
+    }
+    fn convert_targets_to_visits(self, game: &Game, actor_ref: PlayerReference, target_refs: Vec<PlayerReference>) -> Vec<Visit> {
+        crate::game::role::common_role::convert_targets_to_visits(game, actor_ref, target_refs, false, true)
+    }
+    fn get_current_send_chat_groups(self, game: &Game, actor_ref: PlayerReference) -> Vec<ChatGroup> {
+        crate::game::role::common_role::get_current_send_chat_groups(game, actor_ref, vec![ChatGroup::Mafia])
+    }
+    fn get_current_receive_chat_groups(self, game: &Game, actor_ref: PlayerReference) -> Vec<ChatGroup> {
+        crate::game::role::common_role::get_current_receive_chat_groups(game, actor_ref)
+    }
+    fn get_won_game(self, game: &Game, actor_ref: PlayerReference) -> bool {
+        crate::game::role::common_role::get_won_game(game, actor_ref)
+    }
+    fn on_phase_start(self, _game: &mut Game, _actor_ref: PlayerReference, _phase: PhaseType){
+    }
+    fn on_role_creation(self, _game: &mut Game, _actor_ref: PlayerReference){
+        
+    }
+    fn on_any_death(self, _game: &mut Game, _actor_ref: PlayerReference, _dead_player_ref: PlayerReference){
+    }
+    fn on_game_ending(self, _game: &mut Game, _actor_ref: PlayerReference){
+    }
+}
