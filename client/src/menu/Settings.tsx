f27dfab5467d0a924e151f750cbee04c20117a4c
Jack Papel <jackpapel@live.com>
2024-03-23 13:37:43 -0400

    Encyclopedia on settings menu

--- client/src/menu/Settings.tsx
+++ client/src/menu/Settings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import "./settings.css";
 import translate, { LANGUAGES, Language, languageName, switchLanguage } from '../game/lang';
 import GAME_MANAGER from '..';
@@ -8,6 +8,9 @@ import LoadingScreen from './LoadingScreen';
 import { loadSettings, saveSettings } from '../game/localStorage';
 import GameModesEditor from './GameModesEditor';
 import { CopyButton } from '../components/ClipboardButtons';
+import Wiki from '../components/Wiki';
+import { Role } from '../game/roleState.d';
+import { StateListener } from '../game/gameManager.d';
 
 export type Settings = {
     volume: number,
@@ -92,9 +95,43 @@ export default class SettingsMenu extends React.Component<SettingsProps, Setting
                     if(!window.confirm(translate("confirmDelete"))) return;
                     localStorage.clear();
                 }}>{translate('menu.settings.eraseSaveData')}</button>
+                <button onClick={() => {
+                    Anchor.setCoverCard(<WikiCoverCard />);
+                    Anchor.closeSettings();
+                }}>{translate("menu.wiki.title")}</button>
             </div>
         );
     }
+}
+
+function WikiCoverCard(): ReactElement {
+    let defaultDisabledRoles: Role[];
+    switch (GAME_MANAGER.state.stateType) {
+        case "disconnected":
+        case "outsideLobby":
+            defaultDisabledRoles = [];
+        break;
+        case "game":
+        case "lobby":
+            defaultDisabledRoles = GAME_MANAGER.state.excludedRoles;
+        break;
+    }
+    const [disabledRoles, setDisabledRoles] = useState(defaultDisabledRoles);
+
+    useEffect(() => {
+        const listener: StateListener = (type) => {
+            if (type === "excludedRoles" && (GAME_MANAGER.state.stateType === "game" || GAME_MANAGER.state.stateType === "lobby")) {
+                setDisabledRoles(GAME_MANAGER.state.excludedRoles)
+            }
+        }
+        GAME_MANAGER.addStateListener(listener);
+        return () => GAME_MANAGER.removeStateListener(listener);
+    }, []);
+
+    return <div className='wiki-cover-card'>
+        <h1>{translate("menu.wiki.title")}</h1>
+        <Wiki disabledRoles={disabledRoles}/>
+    </div>
 }
 
 export function RoomCodeButton(): JSX.Element {
