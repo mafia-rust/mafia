1fe3fdb869e2643b919ed00458b8aecaa63d4dc6
John-Paul D'Agostino <jp@jpdagostino.com>
2024-06-17 14:55:56 -0400

    Configure local dependencies for mafia user

--- system/install.sh
+++ system/install.sh
@@ -13,14 +13,6 @@ apt-get install -y curl nano sudo git build-essential
 echo Cloning game files
 git clone https://github.com/mafia-rust/mafia.git
 
-echo Installing Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-nvm install 20
-
-echo Installing Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
-rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy
-
 echo Linking Services
 ln -s /mafia/system/mafia-game-client.service /etc/systemd/system/mafia-game-client.service
 ln -s /mafia/system/mafia-game-server.service /etc/systemd/system/mafia-game-server.service
@@ -37,10 +29,14 @@ usermod -aG sudo mafia
 
 echo Setting Permissions
 chmod +x update.sh
+chmod +x install-deps.sh
 chmod +x update-rootless.sh
 chmod +x start-game-client.sh
 chmod +x start-game-server.sh
 chown -R mafia:mafia /mafia
+
+echo Installing Build Dependencies
+runuser -l mafia -c './install-deps.sh'
 
 echo Bootstrapping Mafia
 runuser -l mafia -c './update-rootless.sh'
