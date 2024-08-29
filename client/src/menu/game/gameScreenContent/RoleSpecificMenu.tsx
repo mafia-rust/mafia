b06e595b6a9fa45b93041f80707f2fa791885ac7
Jack Papel <jackpapel@live.com>
2024-08-29 10:42:38 -0400

    RoleSpecificMenu

--- client/src/menu/game/gameScreenContent/RoleSpecificMenu.tsx
+++ client/src/menu/game/gameScreenContent/RoleSpecificMenu.tsx
@@ -1,59 +1,24 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import translate from "../../../game/lang";
-import GAME_MANAGER from "../../../index";
 import { ContentMenu, ContentTab } from "../GameScreen";
-import GameState from "../../../game/gameState.d";
 import RoleSpecificSection from "../../../components/RoleSpecific";
 import SelectionInformation from "../../../components/SelectionInformation";
+import { usePlayerState } from "../../../components/useHooks";
 
-type RoleSpecificMenuProps = {
-}
-type RoleSpecificMenuState = {
-    gameState: GameState,
-}
+export default function RoleSpecificMenu(): ReactElement {
+    const role = usePlayerState(
+        playerState => playerState.roleState.type,
+        ["yourRoleState"]
+    )!;
 
-export default class RoleSpecificMenu extends React.Component<RoleSpecificMenuProps, RoleSpecificMenuState> {
-    listener: () => void;
-    constructor(props: RoleSpecificMenuProps) {
-        super(props);
-
-        if(GAME_MANAGER.state.stateType === "game")
-            this.state = {
-                gameState : GAME_MANAGER.state,
-            };
-        this.listener = ()=>{
-            if(GAME_MANAGER.state.stateType === "game")
-                this.setState({
-                    gameState: GAME_MANAGER.state
-                })
-        };  
-    }
-    componentDidMount() {
-        GAME_MANAGER.addStateListener(this.listener);
-    }
-    componentWillUnmount() {
-        GAME_MANAGER.removeStateListener(this.listener);
-    }
-
-    
-    renderRoleSpecificMenu(){
-        if(this.state.gameState.clientState.type !== "player") return null;
-        return <>
+    return <div className="role-specific-colors">
+        <ContentTab close={ContentMenu.RoleSpecificMenu} helpMenu={null}>
+            {translate("role."+role+".name")}
+        </ContentTab>
+        <div>
             <SelectionInformation />
             <RoleSpecificSection/>
-        </>
-    }
-    render(){
-        if(this.state.gameState.clientState.type === "player")
-            return(
-                <div className="role-specific-colors">
-                    <ContentTab close={ContentMenu.RoleSpecificMenu} helpMenu={null}>
-                        {translate("role."+this.state.gameState.clientState.roleState?.type+".name")}
-                    </ContentTab>
-                    <div>
-                        {this.renderRoleSpecificMenu()}
-                    </div>
-                </div>
-            )
-    }
+        </div>
+    </div>
+    
 }
