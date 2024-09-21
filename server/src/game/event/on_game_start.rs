bb8f2a66b34e6f07d4f8c037ceac71be4f17b476
Sam Maselli <sammypet3@gmail.com>
2024-09-21 05:10:58 -0400

    random love links modifier

--- server/src/game/event/on_game_start.rs
+++ server/src/game/event/on_game_start.rs
@@ -1,9 +1,10 @@
-use crate::game::{components::{cult::Cult, mafia::Mafia, puppeteer_marionette::PuppeteerMarionette}, Game};
+use crate::game::{components::{cult::Cult, mafia::Mafia, puppeteer_marionette::PuppeteerMarionette}, modifiers::Modifiers, Game};
 
 #[must_use = "Event must be invoked"]
 pub struct OnGameStart;
 impl OnGameStart{
     pub fn invoke(game: &mut Game){
+        Modifiers::on_game_start(game);
         Mafia::on_game_start(game);
         Cult::on_game_start(game);
         PuppeteerMarionette::on_game_start(game);
--- server/src/game/modifiers/mod.rs
+++ server/src/game/modifiers/mod.rs
@@ -1,33 +1,39 @@
 use std::collections::HashSet;
 
 use obscured_graves::ObscuredGraves;
+use random_love_links::RandomLoveLinks;
 use serde::{Deserialize, Serialize};
 
 use super::{grave::GraveReference, Game};
 
 pub mod obscured_graves;
+pub mod random_love_links;
 
 #[enum_delegate::register]
-pub trait ModifierTrait{
-    // fn on_game_start(&self, game: &mut Game, event: OnGameStart);
+pub trait ModifierTrait where Self: Clone + Sized{
     fn modifier_type(&self)->ModifierType;
-    fn on_grave_added(self, game: &mut Game, event: GraveReference);
+    fn on_grave_added(self, _game: &mut Game, _event: GraveReference) {}
+    fn on_game_start(self, _game: &mut Game) {}
+
 }
 
 #[enum_delegate::implement(ModifierTrait)]
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub enum ModifierState{
     ObscuredGraves(ObscuredGraves),
+    RandomLoveLinks(RandomLoveLinks),
 }
 #[derive(Clone, Serialize, Deserialize, PartialEq, Eq, Debug, Hash)]
 #[serde(rename_all = "camelCase")]
 pub enum ModifierType{
     ObscuredGraves,
+    RandomLoveLinks
 }
 impl ModifierType{
     pub fn default_state(&self)->ModifierState{
         match self{
             Self::ObscuredGraves => ModifierState::ObscuredGraves(ObscuredGraves::default()),
+            Self::RandomLoveLinks => ModifierState::RandomLoveLinks(RandomLoveLinks::default()),
         }
     }
 }
@@ -47,6 +53,11 @@ impl Modifiers{
     pub fn on_grave_added(game: &mut Game, event: GraveReference){
         for modifier in game.modifiers.modifiers.iter().map(|m| m.clone()).collect::<Vec<_>>(){
             modifier.on_grave_added(game, event);
+        }
+    }
+    pub fn on_game_start(game: &mut Game){
+        for modifier in game.modifiers.modifiers.iter().map(|m| m.clone()).collect::<Vec<_>>(){
+            modifier.on_game_start(game);
         }
     }
 }
