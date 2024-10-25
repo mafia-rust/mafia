e3a502092e2806c3019d63a058dd1dba36ad238f
Sam Maselli <sammypet3@gmail.com>
2024-10-25 19:53:07 -0400

    mortician can tag more than 3

--- client/src/game/roleState.d.tsx
+++ client/src/game/roleState.d.tsx
@@ -133,7 +133,8 @@ export type RoleState = {
     type: "informant",
 } | {
     type: "mortician",
-    obscuredPlayers: PlayerIndex[]
+    obscuredPlayers: PlayerIndex[],
+    cremationsRemaining: number
 } | {
     type: "forger",
     fakeRole: Role,
--- server/src/game/role/mortician.rs
+++ server/src/game/role/mortician.rs
@@ -13,29 +13,35 @@ use crate::game::tag::Tag;
 use crate::game::visit::Visit;
 
 use crate::game::Game;
+use crate::vec_set::VecSet;
 use super::Role;
 use super::{Priority, RoleState, RoleStateImpl};
 
 
-#[derive(Default, Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Mortician {
-    obscured_players: Vec<PlayerReference>
+    obscured_players: VecSet<PlayerReference>,
+    cremations_remaining: u8,
+}
+const MAX_CREMATIONS: u8 = 3;
+impl Default for Mortician {
+    fn default() -> Self {
+        Self {
+            obscured_players: VecSet::new(),
+            cremations_remaining: MAX_CREMATIONS,
+        }
+    }
 }
 
 
 pub(super) const MAXIMUM_COUNT: Option<u8> = Some(1);
 pub(super) const DEFENSE: DefensePower = DefensePower::None;
 
-const MAX_CREMATIONS: u8 = 3;
 
 impl RoleStateImpl for Mortician {
     type ClientRoleState = Mortician;
     fn do_night_action(mut self, game: &mut Game, actor_ref: PlayerReference, priority: Priority) {
-        if Detained::is_detained(game, actor_ref) {return}
-
-        if self.obscured_players.len() as u8 >= MAX_CREMATIONS {return}
-
         match priority {
             Priority::Deception=>{
                 let Some(visit) = actor_ref.night_visits(game).first() else{return};
@@ -43,7 +49,7 @@ impl RoleStateImpl for Mortician {
                 let target_ref = visit.target;
                 
                 if !self.obscured_players.contains(&target_ref){
-                    self.obscured_players.push(target_ref);
+                    self.obscured_players.insert(target_ref);
                     actor_ref.set_role_state(game, RoleState::Mortician(self));
                     actor_ref.push_player_tag(game, target_ref, Tag::MorticianTagged);
                 }
@@ -52,12 +58,11 @@ impl RoleStateImpl for Mortician {
         }
     }
     fn can_select(self, game: &Game, actor_ref: PlayerReference, target_ref: PlayerReference) -> bool {
-        actor_ref != target_ref &&
         !Detained::is_detained(game, actor_ref) &&
+        actor_ref != target_ref &&
         actor_ref.selection(game).is_empty() &&
         actor_ref.alive(game) &&
         target_ref.alive(game) &&
-        (self.obscured_players.len() as u8) < MAX_CREMATIONS && 
         !self.obscured_players.contains(&target_ref)
     }
     fn convert_selection_to_visits(self, game: &Game, actor_ref: PlayerReference, target_refs: Vec<PlayerReference>) -> Vec<Visit> {
@@ -68,15 +73,22 @@ impl RoleStateImpl for Mortician {
             actor_ref.remove_player_tag_on_all(game, Tag::MorticianTagged);
         }
     }
-    fn on_grave_added(self, game: &mut Game, actor_ref: PlayerReference, grave_ref: GraveReference){
-        if actor_ref.alive(game) && self.obscured_players.contains(&grave_ref.deref(game).player) {
+    fn on_grave_added(mut self, game: &mut Game, actor_ref: PlayerReference, grave_ref: GraveReference){
+        if
+            actor_ref.alive(game) &&
+            self.obscured_players.contains(&grave_ref.deref(game).player) &&
+            self.cremations_remaining > 0
+        {
             actor_ref.add_private_chat_message(game, ChatMessageVariant::PlayerRoleAndAlibi{
                 player: grave_ref.deref(game).player,
                 role: grave_ref.deref(game).player.role(game),
                 will: grave_ref.deref(game).player.will(game).to_string(),
             });
+            self.cremations_remaining = self.cremations_remaining.saturating_sub(1);
 
             grave_ref.deref_mut(game).information = GraveInformation::Obscured;
+            
+            actor_ref.set_role_state(game, self);
         }
     }
     fn default_revealed_groups(self) -> std::collections::HashSet<crate::game::components::revealed_group::RevealedGroupID> {
