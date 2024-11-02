5b20ce18e8842ec0643362b8c1d5a0334a32beb7
Sam Maselli <sammypet3@gmail.com>
2024-11-02 03:05:24 -0400

    chat virtualization

--- client/src/components/NightMessagePopup.tsx
+++ client/src/components/NightMessagePopup.tsx
@@ -2,21 +2,22 @@ import React, { ReactElement } from 'react';
 import ChatElement, { ChatMessage } from './ChatMessage';
 import translate from '../game/lang';
 import "./nightMessagePopup.css";
+import { Virtuoso } from 'react-virtuoso';
 
 export default function NightMessagePopup(props: Readonly<{
     messages: ChatMessage[]
 }>): ReactElement {
+
     return <div className="chat-menu chat-menu-colors night-message-popup">
         <h2>{translate("nightMessages")}</h2>
-        <div className="chat-message-section">
-            <div className="chat-message-list">
-                {props.messages.map((msg, index) => {
-                    return <ChatElement
-                        key={index}
-                        message={msg}
-                    />;
-                })}
-            </div>
-        </div>
+        <Virtuoso
+            alignToBottom={true}
+            totalCount={props.messages.length}
+            followOutput={'smooth'}
+            itemContent={(index) => <ChatElement
+                key={index}
+                message={props.messages[index]}
+            />}
+        />
     </div>
 }
