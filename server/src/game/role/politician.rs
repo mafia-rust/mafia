d26355849835966dfbfc250b3d794161e9cb26ed
Sam Maselli <sammypet3@gmail.com>
2024-11-08 19:12:38 -0500

    politician skip phase and state

--- server/src/game/role/politician.rs
+++ server/src/game/role/politician.rs
@@ -20,8 +20,36 @@ use super::{GetClientRoleState, RoleState, RoleStateImpl};
 #[derive(Debug, Clone, Default)]
 pub struct Politician{
     pub revealed: bool,
-    countdown_started: bool,
-    countdown_nomination_started: bool,
+    state: PoliticianState,
+}
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+pub enum PoliticianState{
+    #[default]
+    None,
+    CountdownStarted,
+    FinalNomination,
+    Finished
+}
+impl PoliticianState {
+    pub fn is_none(&self) -> bool {
+        matches!(self, Self::None)
+    }
+    pub fn countdown_started(&self) -> bool {
+        match self {
+            PoliticianState::None => false,
+            PoliticianState::CountdownStarted => true,
+            PoliticianState::FinalNomination => true,
+            PoliticianState::Finished => false,
+        }
+    }
+    pub fn final_nomination(&self) -> bool {
+        match self {
+            PoliticianState::None => false,
+            PoliticianState::CountdownStarted => false,
+            PoliticianState::FinalNomination => true,
+            PoliticianState::Finished => false,
+        }
+    }
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -59,55 +87,40 @@ impl RoleStateImpl for Politician {
     }
 
     fn on_phase_start(mut self, game: &mut Game, actor_ref: PlayerReference, phase: PhaseType){
-        if
-            actor_ref.alive(game) &&
-            PlayerReference::all_players(game)
-                .filter(|p|p.alive(game))
-                .filter(|p|p.keeps_game_running(game))
-                .all(|p|
-                    !p.win_condition(game).is_loyalist_for(GameConclusion::Town)
-                )
+        Self::check_and_leave_town(&self, game, actor_ref);
 
-        {
-            actor_ref.die(game, Grave::from_player_leave_town(game, actor_ref));
-        }
+        if self.state.countdown_started() && actor_ref.alive(game) {
+            //for skipping phases
+            match phase {
+                PhaseType::Briefing | PhaseType::Nomination | PhaseType::Testimony | 
+                PhaseType::Judgement | PhaseType::FinalWords  => {}
 
-        if self.countdown_started {
+                PhaseType::Obituary | PhaseType::Discussion | PhaseType::Dusk | PhaseType::Night => {
+                    game.phase_machine.time_remaining = Duration::from_secs(0);
+                }
+            }
+
             match phase {
                 PhaseType::Nomination => {
-                    self.countdown_nomination_started = true;
+                    self.state = PoliticianState::FinalNomination;
                     actor_ref.set_role_state(game, self);
                 },
                 PhaseType::Dusk => {
-                    if self.countdown_nomination_started {
+                    if self.state == PoliticianState::FinalNomination {
                         Politician::kill_all(game);
                     }
                 },
-                _ => {
-                    game.phase_machine.time_remaining = Duration::from_secs(0);
-                }
+                _ => {}
             }
         }
     }
     
-    fn on_any_death(mut self, game: &mut Game, actor_ref: PlayerReference, _dead_player_ref: PlayerReference){
-        if _dead_player_ref == actor_ref || self.countdown_started {
-            return; 
-        }
-        self.countdown_started = actor_ref.alive(game) &&
-            PlayerReference::all_players(game)
-            .filter(|p|*p != actor_ref)
-            .filter(|p|p.keeps_game_running(game))
-            .filter(|p|p.alive(game))
-            .any(|player| {
-                player.win_condition(game).is_loyalist_for(GameConclusion::Town)
-            });
-        
-        if self.countdown_started {
-            Politician::start_countdown(&mut self, game);
-        }
+    fn on_any_death(self, game: &mut Game, actor_ref: PlayerReference, _dead_player_ref: PlayerReference){
+        self.check_and_start_countdown(game, actor_ref);
+    }
 
-        actor_ref.set_role_state(game, self);
+    fn on_role_creation(self, game: &mut Game, actor_ref: PlayerReference) {
+        self.check_and_start_countdown(game, actor_ref);
     }
 
     fn default_win_condition(self) -> crate::game::win_condition::WinCondition where RoleState: From<Self> {
@@ -122,13 +135,54 @@ impl GetClientRoleState<ClientRoleState> for Politician {
 }
 
 impl Politician {
+    fn check_and_leave_town(&self, game: &mut Game, actor_ref: PlayerReference){
+        if
+            !self.state.countdown_started() &&
+            actor_ref.alive(game) &&
+            PlayerReference::all_players(game)
+                .filter(|p|p.alive(game))
+                .filter(|p|p.keeps_game_running(game))
+                .all(|p|
+                    !p.win_condition(game).is_loyalist_for(GameConclusion::Town)
+                )
+
+        {
+            actor_ref.die(game, Grave::from_player_leave_town(game, actor_ref));
+        }
+    }
+
+    fn check_and_start_countdown(mut self, game: &mut Game, actor_ref: PlayerReference){
+        if !actor_ref.alive(game) || self.state.countdown_started() {
+            return; 
+        }
+        
+        if Self::should_start_countdown(&self, game, actor_ref) {
+            Politician::start_countdown(&mut self, game);
+        }
+
+        actor_ref.set_role_state(game, self);
+    }
+
+
+    fn should_start_countdown(&self, game: &Game, actor_ref: PlayerReference)->bool{
+        !self.state.countdown_started() &&
+        actor_ref.alive(game) &&
+            PlayerReference::all_players(game)
+            .filter(|p|*p != actor_ref)
+            .filter(|p|p.keeps_game_running(game))
+            .filter(|p|p.alive(game))
+            .all(|player| {
+                player.win_condition(game).is_loyalist_for(GameConclusion::Town)
+            })
+    }
+
     fn start_countdown(&mut self, game: &mut Game){
         game.add_message_to_chat_group(ChatGroup::All, ChatMessageVariant::PoliticianCountdownStarted);
         
         if game.current_phase().phase() != PhaseType::Nomination {
             game.phase_machine.time_remaining = Duration::from_secs(0);
         }
-        self.countdown_started = true;
+        self.state = PoliticianState::CountdownStarted;
     }
 
     fn kill_all(game: &mut Game){
