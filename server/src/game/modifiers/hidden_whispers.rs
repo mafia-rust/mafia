e0b2102004654edcc6520a6c727cdfc25df6d821
Apersoma <95072312+Apersoma@users.noreply.github.com>
2025-03-01 13:24:28 -0500

    Added modifier server side

--- server/src/game/modifiers/hidden_whispers.rs
+++ server/src/game/modifiers/hidden_whispers.rs
@@ -3,6 +3,9 @@ use super::{ModifierTrait, ModifierType};
 #[derive(Clone, Default, PartialEq, Eq, Hash)]
 pub struct HiddenWhispers;
 
+/*
+    There is modifier specific code in the player_send_packet::send_chat_messages() function
+*/
 impl From<&HiddenWhispers> for ModifierType{
     fn from(_: &HiddenWhispers) -> Self {
         ModifierType::HiddenWhispers
