026267a337e14d1638e79a5259de64d59e69cd51
sammy <sammypet3@gmail.com>
2025-03-06 18:01:23 +0000

    skip some of day 2, so game starts at dusk 2

--- server/src/game/modifiers/skip_day_1.rs
+++ server/src/game/modifiers/skip_day_1.rs
@@ -1,4 +1,4 @@
-use crate::game::{phase::{PhaseState, PhaseType}, Game};
+use crate::game::{event::on_fast_forward::OnFastForward, phase::{PhaseState, PhaseType::*}, Game};
 
 use super::{ModifierTrait, ModifierType};
 
@@ -13,8 +13,17 @@ impl From<&SkipDay1> for ModifierType{
 
 impl ModifierTrait for SkipDay1{
     fn on_phase_start(self, game: &mut Game, phase: PhaseState) {
-        if game.day_number() != 1 {return;}
-        if phase.phase() == PhaseType::Briefing {return;}
-        game.on_fast_forward();
+        match (phase.phase(), game.day_number()) {
+            (Dusk, 1) |
+            (Night, 1) |
+            (Obituary, 2) |
+            (Discussion, 2) |
+            (Nomination, 2) |
+            (Testimony, 2) |
+            (Judgement, 2) |
+            (FinalWords, 2)
+                => OnFastForward::invoke(game),
+            _ => ()
+        }
     }
 }
