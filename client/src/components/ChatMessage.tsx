fb309fc8a6f1dc50168083685e735eb2a1ecfb92
Apersoma <95072312+Apersoma@users.noreply.github.com>
2025-03-06 21:32:06 -0500

    added wiki entry. renamed messages on client side

--- client/src/components/ChatMessage.tsx
+++ client/src/components/ChatMessage.tsx
@@ -604,8 +604,8 @@ export function translateChatMessage(
             return translate("chatMessage.godfatherBackupKilled", playerNames[message.backup]);
         case "roleBlocked":
             return translate("chatMessage.roleBlocked" + (message.immune ? ".immune" : ""));
-        case "sheriffResult":
-            return translate("chatMessage.sheriffResult." + (message.suspicious ? "suspicious" : "innocent"));
+        case "detectiveResult":
+            return translate("chatMessage.detectiveResult." + (message.suspicious ? "suspicious" : "innocent"));
         case "snoopResult":
             return translate("chatMessage.snoopResult." + (message.townie ? "townie" : "inconclusive"));
         case "gossipResult":
@@ -620,8 +620,8 @@ export function translateChatMessage(
             return translate("chatMessage.spyBug."+message.bug);
         case "trackerResult":
             return translate("chatMessage.trackerResult", playerListToString(message.players, playerNames));
-        case "seerResult":
-            return translate("chatMessage.seerResult." + (message.enemies ? "enemies" : "friends"));
+        case "philosopherResult":
+            return translate("chatMessage.philosopherResult." + (message.enemies ? "enemies" : "friends"));
         case "psychicEvil":
             return translate(
                 "chatMessage.psychicEvil",
@@ -938,7 +938,7 @@ export type ChatMessageVariant = {
 {
     type: "wardblocked"
 } | {
-    type: "sheriffResult", 
+    type: "detectiveResult", 
     suspicious: boolean
 } | {
     type: "snoopResult", 
@@ -962,7 +962,7 @@ export type ChatMessageVariant = {
     type: "trackerResult",
     players: PlayerIndex[]
 } | {
-    type: "seerResult",
+    type: "philosopherResult",
     enemies: boolean
 } | {
     type: "psychicGood",
