478a0f1c7c44c416515d85e71a98a6092c475296
Lyx <lyx@hustonfam.net>
2025-03-11 20:12:47 -0400

    update coward to redirect attacks instead of protecting themself and attacking the target.
    Also, fixed obscuring.

--- server/src/game/role/coward.rs
+++ server/src/game/role/coward.rs
@@ -10,14 +10,13 @@ use crate::game::player::PlayerReference;
 
 use crate::game::visit::Visit;
 use crate::game::Game;
-use crate::vec_set::VecSet;
 use super::{
 	ControllerID, ControllerParametersMap, GetClientRoleState, Priority, Role, RoleStateImpl
 };
 
 #[derive(Clone, Debug, Default)]
 pub struct Coward {
-	pub tagged_for_obscure: VecSet<PlayerReference>
+	pub obscure: Option<PlayerReference>
 }
 
 #[derive(Clone, Serialize, Debug)]
@@ -32,52 +31,55 @@ impl RoleStateImpl for Coward {
 
     fn do_night_action(self, game: &mut Game, actor_ref: PlayerReference, priority: Priority) {
         
+    	if priority != Priority::Heal {return;}
+    
     	let actor_visits = actor_ref.untagged_night_visits_cloned(game);
     
-	   	match priority {
-	        Priority::Kill => {
-	
-	        if let Some(target) = actor_visits.first(){
-		       	actor_ref.push_night_message(game, 
-		            ChatMessageVariant::CowardHid
+    	if let Some(target) = actor_visits.first(){
+	       	actor_ref.push_night_message(game, 
+	            ChatMessageVariant::CowardHid
+	        );
+			
+			let mut saved : bool = false;
+			let new_visits = NightVisits::all_visits(game).into_iter().copied().collect::<Vec<_>>();
+			for mut visit in new_visits {
+                if 
+                    visit.attack &&
+                    visit.target == actor_ref &&
+                    visit.visitor != actor_ref
+                {
+                	visit.target = target.target;
+                 	saved = true;
+                }
+            }
+            
+            for player_ref in PlayerReference::all_players(game){
+                if player_ref == actor_ref {continue;}
+                if player_ref.role(game) == Role::Transporter {continue;}
+    
+    
+                let new_visits = player_ref.all_night_visits_cloned(game).clone().into_iter().map(|mut v|{
+                    if
+                    	v.attack &&
+                    	v.target == actor_ref &&
+                    	v.visitor != actor_ref {
+                     	saved = true;
+                        v.target = target.target;
+                    }
+                    v
+                }).collect();
+                player_ref.set_night_visits(game, new_visits);
+                
+                let obscure = Some(target.target);
+                actor_ref.set_role_state(game, Coward{obscure});
+            }
+            
+            if saved {
+	           	actor_ref.push_night_message(game, 
+		            ChatMessageVariant::CowardSaved
 		        );
-	        
-				let mut tagged_for_obscure = self.tagged_for_obscure.clone();
-				
-				let mut saved : bool = false;
-				let mut attack_success : bool = false;
-				for visit in NightVisits::all_visits(game).into_iter().copied().collect::<Vec<_>>() {
-	                if 
-	                    visit.attack &&
-	                    visit.target == actor_ref &&
-	                    visit.visitor != actor_ref
-	                {
-	                	saved = true;
-	                	attack_success |= target.visitor.try_night_kill_single_attacker(
-	                 		actor_ref, game, GraveKiller::Role(Role::Engineer), AttackPower::ProtectionPiercing, false
-	                 	);
-	                }
-	            }
-	            if attack_success {
-		            tagged_for_obscure.insert(target.visitor);
-		        }
-	            if saved {
-		           	actor_ref.push_night_message(game, 
-			            ChatMessageVariant::CowardSaved
-			        );
-	            }
-				
-	            actor_ref.set_role_state(game, Coward{tagged_for_obscure});
-	        }
-	
-	        }
-	        Priority::Heal => {
-	            if let Some(_) = actor_visits.first(){
-	            	actor_ref.increase_defense_to(game, DefensePower::Protection);
-	            }
-	        }
-	        _ => {}
-	    }
+            }
+        }
     }
     
     fn controller_parameters_map(self, game: &Game, actor_ref: PlayerReference) -> ControllerParametersMap {
@@ -100,9 +102,17 @@ impl RoleStateImpl for Coward {
     }
     
     fn on_grave_added(self, game: &mut Game, _: PlayerReference, grave_ref: GraveReference){
-        if !self.tagged_for_obscure.contains(&grave_ref.deref(game).player) {return}
-
-        grave_ref.deref_mut(game).information = GraveInformation::Obscured;
+    	if let Some(player) = self.obscure {
+        	if player == grave_ref.deref(game).player {
+        	 	grave_ref.deref_mut(game).information = GraveInformation::Obscured;
+         	}
+        }
+    }
+    fn on_phase_start(self, _game: &mut Game, _actor_ref: PlayerReference, _phase: crate::game::phase::PhaseType) {
+    	if let crate::game::phase::PhaseType::Night = _phase {
+	   		let obscure = None;
+	     	_actor_ref.set_role_state(_game, Coward{obscure});
+     	}
     }
 }
 
