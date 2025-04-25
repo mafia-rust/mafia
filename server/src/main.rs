2778829c4070d412247030b143301a5d09e786cc
sammy <sammypet3@gmail.com>
2025-04-25 08:21:19 +0000

    environment variables

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,7 @@
 
 use mafia_server::{log, websocket_connections::websocket_server::create_ws_server};
-use serde::Deserialize;
-use std::{fs, thread, time::Duration};
+use std::{thread, time::Duration};
 
-#[derive(Deserialize)]
-struct Config{
-    address: String,
-}
 
 ///
 /// The Main function
@@ -16,14 +11,12 @@ struct Config{
 /// 
 #[tokio::main]
 async fn main() -> ! {
-    let path_str = "./resources/config.json";
-    let config = serde_json::from_str::<Config>(
-        &fs::read_to_string(path_str)
-            .unwrap_or_else(|_| panic!("Failed to read the config file \"{}\"", path_str))
-    ).unwrap();
+
+    dotenv::dotenv().ok();
+    let address = std::env::var("WS_ADDRESS").expect("Missing environment variabled WS_ADDRESS");
 
     loop {
-        create_ws_server(&config.address).await;
+        create_ws_server(&address).await;
         // This delay is only to make sure disconnect messages are sent before the server restarts
         thread::sleep(Duration::from_secs(1));
         log!(important "Server"; "Restarting...");
