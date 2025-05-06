6ce0d361dc9aca0894e7ce56274df84b5d7d35f1
Apersoma <95072312+Apersoma@users.noreply.github.com>
2025-05-06 15:59:42 -0400

    v3

--- client/src/components/gameModeSettings/EnabledModifiersSelector.tsx
+++ client/src/components/gameModeSettings/EnabledModifiersSelector.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useContext, useState } from "react";
 import { ModifierType, toModifierType } from "../../game/gameState.d";
 import translate from "../../game/lang";
+import "./enabledModifiersSelector.css"
 import { GameModeContext } from "./GameModesEditor";
 import CheckBox from "../CheckBox";
 import Select, { SelectOptionsSearch, set_options_typical } from "../Select";
@@ -55,8 +56,12 @@ type EnabledModifiersDisplayProps = {
 )
 
 export function EnabledModifiersDisplay(props: EnabledModifiersDisplayProps): ReactElement { 
-    function select<K extends string>(options: (ModifierType | K)[]): ReactElement {
-        function modifiersOnly<K extends string>(options: SelectOptionsSearch<ModifierType | K>): ModifierType[] {
+    let leftSideOptions: string[] = [""];
+    function select<K extends string>(options: (ModifierType | K)[], name: string, leftSide: boolean): ReactElement {
+        if(leftSide){
+            leftSideOptions.concat(options)
+        }
+        function modifiersOnly(options: SelectOptionsSearch<ModifierType | K>): ModifierType[] {
             let result: ModifierType[] = [];
             for(const option of options){
                 const modifier = toModifierType(option[0]);
@@ -66,7 +71,7 @@ export function EnabledModifiersDisplay(props: EnabledModifiersDisplayProps): Re
             }
             return result
         }
-        function selectedOption<K extends string>(options: SelectOptionsSearch<ModifierType | K>): ModifierType | K | "error" {
+        function selectedOption(options: SelectOptionsSearch<ModifierType | K>): ModifierType | K | "error" {
             let def: K | ModifierType | "error" = "error";
             for(const option of options) {
                 const modifier = toModifierType(option[0]);
@@ -82,39 +87,47 @@ export function EnabledModifiersDisplay(props: EnabledModifiersDisplayProps): Re
         const optionsMap: SelectOptionsSearch<ModifierType | K> = new Map();
         set_options_typical(optionsMap, options);
 
-        return <Select 
-            value={selectedOption(optionsMap)}
-            onChange={value => {
-                if(props.modifiable === true){
-                    const modifier = toModifierType(value);
-                    if(modifier !== undefined) {
-                        props.onEnableModifiers([modifier])
-                    } else {
-                        props.onDisableModifiers(modifiersOnly(optionsMap));
+        return <>
+            <td style={{paddingRight:"1em", textAlign:"right"}}>{translate(name)}</td>
+            <td style={{textAlign:"left"}}><Select 
+                value={selectedOption(optionsMap)}
+                onChange={value => {
+                    if(props.modifiable === true){
+                        const modifier = toModifierType(value);
+                        if(modifier !== undefined) {
+                            props.onEnableModifiers([modifier])
+                        } else {
+                            props.onDisableModifiers(modifiersOnly(optionsMap));
+                        }
                     }
-                }
-            }}
-            optionsSearch = {optionsMap}
-        />
+                }}
+                optionsSearch = {optionsMap}
+            /></td>
+        </>
     }
     function checkBox(modifier: ModifierType, inverted: boolean): ReactElement {
-        return <CheckBox 
-            checked={props.enabledModifiers.includes(modifier)} 
-            onChange={checked => {if(props.modifiable) {
-                if(checked !== inverted){
-                    props.onEnableModifiers([modifier])
-                } else {
-                    props.onDisableModifiers([modifier])
+        return <>
+            <td style={{paddingRight:"1em", textAlign:"right"}}>{translate("modifierMenu."+modifier)}</td>
+            <td style={{textAlign:"left"}}><CheckBox
+            checked={props.enabledModifiers.includes(modifier) !== inverted}
+            onChange={checked => {
+                if (props.modifiable) {
+                    if (checked !== inverted) {
+                        props.onEnableModifiers([modifier]);
+                    } else {
+                        props.onDisableModifiers([modifier]);
+                    }
                 }
-            }}}
-        />
+            } } /></td>
+        </>
     }
     const [hideDisabled, setHideDisabled] = useState(true);
 
-    const leftSideOptions: string[] = ["scheduledNominations", "unscheduledNominations", "noTrialPhases", "popularVote", "twoThirdsMajority", "autoGuilty"];
     let longestLeftSideOption: number = 10;
     leftSideOptions.forEach(option => {longestLeftSideOption = Math.max(longestLeftSideOption, option.length)})
     const padding: string = longestLeftSideOption.toString() +"ch";
+    console.log("fuck1 "+padding)
+    console.log("fuck2 "+leftSideOptions.toString())
 
     const voteOptionMap: SelectOptionsSearch<ModifierType | "popularVote"> = new Map();
     set_options_typical(voteOptionMap, ["popularVote", "twoThirdsMajority", "autoGuilty"])
@@ -130,38 +143,28 @@ export function EnabledModifiersDisplay(props: EnabledModifiersDisplayProps): Re
             />
         </label>}
         <div>
-            <table>
-                <tr>
-                    <td></td>
-                    <td style = {{paddingRight: padding}}></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+            <table className="modifier-table">
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td style = {{paddingRight: padding}}></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </thead>
                 <tbody>
-                    <tr>        {/* Trial Phases            |   Chat            */}
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Trial Phases:</td>
-                        <td style={{textAlign:"left"}}>{select(["scheduledNominations", "unscheduledNominations", "noTrialPhases"])}</td>
-        
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Chat:</td>
-                        <td style={{textAlign:"left"}}>{select(["allChat", "noNightChat", "noChat"])}</td>
-                    </tr><tr>   {/* Guilty Vote Requirement |   Dead Can Chat   */}
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Guilty Vote Requirement:</td>
-                        <td style={{textAlign:"left"}}>{select(["popularVote", "twoThirdsMajority", "autoGuilty"])}</td>
-
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Dead Can Chat:</td>
-                        <td style={{textAlign:"left"}}>{checkBox("deadCanChat", false)}</td>
-                    </tr><tr>   {/* Allow Abstaining        |   Whispers        */}
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Allow Abstaining:</td>
-                        <td style={{textAlign:"left"}}>{checkBox("abstaining", false)}</td>
-                        
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Whispers:</td>
-                        <td style={{textAlign:"left"}}>{select(["broadcastWhispers", "hiddenWhispers", "noWhispers"])}</td>
-                    </tr><tr>   {/* Skip Day 1              |   Graves          */}
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Skip Day 1:</td>
-                        <td style={{textAlign:"left"}}>{checkBox("skipDay1", false)}</td>
-
-                        <td style={{paddingRight: "1em", textAlign: "right"}}>Graves:</td>
-                        <td style={{textAlign:"left"}}>{select(["normalGraves", "noDeathCause", "roleSetGraveKillers", "obscuredGraves"])}</td>
+                    <tr>{     /* Trial Phases            |   Chat            */}
+                        {select(["modifierMenu.fake.scheduledNominations", "unscheduledNominations", "noTrialPhases"], "modifierMenu.trialPhases", true)}
+                        {select(["modifierMenu.fake.allChat", "noNightChat", "noChat"], "modifierMenu.chat", false)}
+                    </tr><tr>{/* Guilty Vote Requirement |   Dead Can Chat   */}
+                        {select(["modifierMenu.fake.popularVote", "twoThirdsMajority", "autoGuilty"], "modifierMenu.guiltyVoteRequirement", true)}
+                        {checkBox("deadCanChat", false)}
+                    </tr><tr>{/* Allow Abstaining        |   Whispers        */}
+                        {checkBox("abstaining", false)}
+                        {select(["modifierMenu.fake.broadcastWhispers", "hiddenWhispers", "noWhispers"], "modifierMenu.whispers", false)}
+                    </tr><tr>{/* Skip Day 1              |   Graves          */}
+                        {checkBox("skipDay1", false)}
+                        {select(["modifierMenu.fake.normalGraves", "noDeathCause", "roleSetGraveKillers", "obscuredGraves"], "modifierMenu.graves", false)}
                     </tr>
                 </tbody>
             </table>
